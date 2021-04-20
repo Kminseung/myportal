d78d825f969115a5128e6fb73ad8763f92ea42b1
Kminseung <soccer6504@gmail.com>
2021-04-20 13:05:10 +0900

    Validator Version Fix

--- myportal/src/main/java/com/bitacademy/myportal/vo/MemberVo.java
+++ myportal/src/main/java/com/bitacademy/myportal/vo/MemberVo.java
@@ -2,11 +2,24 @@ package com.bitacademy.myportal.vo;
 
 import java.util.Date;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
 public class MemberVo {
 	// 필드
 	private Long no;
+	
+	@NotEmpty		// name 필드는 null이 아니여야 하고
+	@Length(min=2, max=8)			// 길이가 2글자 ~ 8글자
 	private String name;
+	
+	@NotEmpty		// null이 아니여야 하고
+	@Email			// 이메일 형식이어야 함
 	private String email;
+	
+	@NotEmpty		// password 필드는 null이 아니여야 하고
+	@Length(min=4, max=20)		// 길이가 4~20 글자
 	private String password;
 	private String gender;
 	private Date joinDate;
